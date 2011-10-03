05a268764c18cfe734c715f9b5a24f27e7260fef
billy <billy.rennekamp@gmail.com>
2011-10-02 21:12:03 -0400

    current stable

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+
+
+
+
+	$hide = array(	'resources',
+					'index.php',
+					'.htaccess',
+					'.htpasswd',
+					'.DS_Store');
+			
+	error_reporting(E_ERROR);
+	
+	
+	$filepath = $_SERVER['SCRIPT_FILENAME'];
+	$scriptname = basename($filepath);
+	$readpath = str_replace($scriptname, "", $filepath)."/secret";
+
+	$handle = opendir($readpath);
+	
+
+	while ($file = readdir($handle)) { 
+		
+		if ($file == "." || $file == ".." || in_array($file, $hide))  continue;
+		
+
+		
+		$files[] = $file;
+		
+	}
+
+	closedir($handle); 
+
+	// Sort our files
+	@ksort($files, SORT_NUMERIC);
+	$files = @array_reverse($files);
+
+?>
+	
+	<!DOCTYPE html>
+	<html>
+	
+		<head>
+			
+			<title>Artist Name</title>
+
+			
+		</head>
+		
+		<body style="padding:10px; font-size:11pt;">
+
+			Artist Name<br>
+			<sub><a href="mailto:mail@gmail.com">mail</a> - <a href="./secret/CV.pdf">CV</a></sub>
+<br><br><br><br><br><br><br><br><br>
+	  	
+			<?php $baseurl = $_SERVER['PHP_SELF']; ?>
+
+			<table border="0" cellspacing="5" cellpadding="5">
+
+				<?php
+					$arsize = sizeof($files);
+
+					//print_r($files);
+					//echo"<br>";				
+
+					for ($i=0; $i<$arsize; $i++) {
+					
+						$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
+						$filename = str_replace(".".$ext, "",stripslashes($files[$i]));
+						$fileURL = "./secret/".stripslashes($files[$i]);
+
+						//echo"filename = $filename - fileurl = $fileurl - file = ".$files[$i]."<br>";
+					$infoArray = explode("_",$filename);
+					$count = count($infoArray);
+					//print_r($infoArray);
+					//echo"<br>";
+					$order = $infoArray[0];
+					$title = $infoArray[1];
+					$side = $infoArray[2];
+					$sideSay = ($side == "l" ? "left" : ($side == "r" ? "right" : "center"));
+
+					$end = "</div><br><br><br><br><br><br>";
+					$start = "<div style=\"text-align:$sideSay;\">";
+					if ($order == $lastOrder){$end = "";$start = "";}
+					$lastOrder = $order;
+					if($i==0){$lastOrder = $order;}
+					else{echo $end;}
+
+
+					if ($ext == "jpg" || $ext == "gif" || $ext == "png" || $ext == "jpeg"):	
+					if ($count == 3):			
+				?>
+				<?php echo $start; ?><img style="padding:10px;" alt="<?php echo $title; ?>" src="<?php echo $fileURL; ?>">		
+				<?php
+				echo"\n";
+				endif;
+				endif;
+ } ?>
+			
+			</table>
+	
+
+
+		</body>
+		
+	</html>
---
+++ secret/index.php
@@ -0,0 +1,166 @@
+<?php
+
+
+
+
+$hide = array(	'resources',
+	'index.php',
+	'.htaccess',
+	'.htpasswd',
+	'.DS_Store');
+
+error_reporting(E_ERROR);
+
+if (stripslashes($_GET['download']) ) {
+	$file = str_replace('/', '', $_GET['download']);
+	$file = str_replace('..', '', $file);
+
+	if (file_exists($file)) {
+		header("Content-type: application/x-download");
+		header("Content-Length: ".filesize($file)); 
+		header('Content-Disposition: attachment; filename="'.$file.'"');
+		readfile($file);
+		die();
+	}
+}
+
+$filepath = $_SERVER['SCRIPT_FILENAME'];
+$scriptname = basename($filepath);
+$readpath = str_replace($scriptname, "", $filepath);
+$handle = opendir($readpath);
+
+$password = "password"
+
+
+//DELETE
+if (isset($_GET['rmfile']) && stripslashes($_REQUEST['pw']) == $password) {
+	unlink($readpath . $_GET['rmfile']);
+}
+
+//UPLOAD
+if ($_FILES['file'] && stripslashes($_REQUEST['pw']) == $password) {
+	$ext = strtolower(substr($_FILES['file']['name'], strrpos($_FILES['file']['name'], '.')+1));
+	if ($_REQUEST['order'] != "")
+	{
+		$success = move_uploaded_file($_FILES['file']['tmp_name'], stripslashes($_REQUEST['order'])."_".str_replace("'","",stripslashes($_REQUEST['title']))."_".stripslashes($_REQUEST['side']).".".$ext);
+	}
+	else
+	{
+		$success = move_uploaded_file($_FILES['file']['tmp_name'], str_replace("'","",stripslashes($_FILES['file']['name'])));
+	}
+}
+
+while ($file = readdir($handle)) { 
+
+	if ($file == "." || $file == ".." || in_array($file, $hide))  continue;
+
+	$key = @filemtime($file);
+
+	$files[$key] = $file;
+
+}
+
+closedir($handle); 
+
+// SORT
+@ksort($files, SORT_NUMERIC);
+$files = @array_reverse($files);
+
+?>
+
+	<!DOCTYPE html>
+	<html>
+
+	<head>
+
+	<title>Secret Stuff</title>
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+	<script src="./jquery-1.5.js"></script>
+<script type="text/javascript">
+	function deleteFile(filename)
+{
+	password = $("#pw").val();
+	var url = "./index.php?rmfile="+filename+"&pw="+password;   
+	if (confirm("delete "+filename+"?"))
+	{ 
+		$(location).attr('href',url);
+	}
+}
+function checkForm()
+{
+	side = $("#side");
+	order = $("#order");
+	title = $("#title");
+	file = $("#file");
+	var thisext = file.val().substr(file.val().lastIndexOf('.'));
+	if (thisext == ".jpg" || thisext == ".jpeg" || thisext == ".gif" || thisext == ".png" )
+	{
+		if (side.val() != "l" && side.val() != "r" && side.val() != "c")
+		{
+			side.css("background","red");
+			alert('you fucked up side');
+			return false;
+		}
+		else if (isNaN(order.val()))
+		{
+			order.css("background","red");
+			alert('you fucked up order');
+			return false;	
+		}
+		else if (title.val() == "")
+		{
+			title.css("background","red");
+			alert('you fucked up title');
+			return false;	
+		}
+		else
+		{
+			return true;
+		}
+	}
+	else
+	{
+		return true;
+	}
+}
+</script>			
+	</head>	
+	<body>
+	<?php $baseurl = $_SERVER['PHP_SELF']; ?>
+<table border="0" cellspacing="5" cellpadding="5">
+	<?php
+$arsize = sizeof($files);	
+for ($i=0; $i<$arsize; $i++) {	
+	$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
+	$filename = stripslashes($files[$i]);
+	$fileurl = $files[$i];
+	if (strlen($filename) > 43) 
+	{
+		$filename = substr($files[$i], 0, 40) . '...';
+	}
+	?>
+		<tr>
+		<td>-</td>
+		<td><a href="./index.php?download=<?php echo $filename; ?>"><?php echo $filename; ?></a></td>
+	<td><?php echo round(filesize($leadon.$files[$i])/1024); ?>KB</td>
+	<td><?php echo date ("d/m/y", filemtime($leadon.$files[$i]));?></td>
+	<td><a onClick="deleteFile('<?php echo $filename; ?>'); return false;" href="#">Delete</a></td>
+	</tr>
+		<?php } ?>
+	</table>
+
+		<div id="upload">
+		<form method="post" onSubmit="return checkForm();" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
+	<p><input type="text" id="side" name="side"></input> L, R, or C</p>
+		<p><input type="text" id="order" name="order"></input> order</p>
+		<p><input type="text" id="title" name="title"></input> title</p>
+		<p><input type="text" id="pw" name="pw" value="<?php echo stripslashes($_REQUEST['pw']) ?>"></input> password</p>
+	<p><input type="file" id="file" name="file" /></p>
+		<p><input type="submit" value="Upload" /></p>
+		</form>
+
+		</div>
+
+		</body>
+
+		</html>
