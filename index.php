1c6f6a67ba49f3d30d16174a18b2f4ae831afba6
billy <billy.rennekamp@gmail.com>
2011-10-02 23:13:41 -0400

    fade in titles

--- index.php
+++ index.php
@@ -44,17 +44,35 @@
 		<head>
 	<link rel="stylesheet" type="text/css" href="styles.css" />
 	<script src="jquery-1.5.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+	$(".piece").hover(
+		function(){
+
+			$("#_"+this.id).animate({opacity:1},500,function(){});
+			console.log($("#_"+this.id));
+	},
+		function(){
+			$("#_"+this.id).animate({opacity:0},500,function(){});
+
+	});
+
+});
+
+</script>
 			
 			<title>Artist Name</title>
 
 			
 		</head>
 		
-		<body style="padding:10px; font-size:11pt;">
+		<body style="padding:10px; font-size:11pt; padding-top:160px;">
 
-			Artist Name<br>
+			<div id="header"><a href=".">Artist Name</a><br>
 			<sub><a href="mailto:mail@gmail.com">mail</a> - <a href="./secret/CV.pdf">CV</a></sub>
-<br><br><br><br><br><br><br><br><br>
+			</div>
+<!--<br><br><br><br><br><br><br><br><br>-->
 	  	
 			<?php $baseurl = $_SERVER['PHP_SELF']; ?>
 
@@ -65,7 +83,7 @@
 
 					//print_r($files);
 					//echo"<br>";				
-
+					$end = "</div><br><br><br><br><br><br>";
 					for ($i=0; $i<$arsize; $i++) {
 					
 						$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
@@ -81,19 +99,19 @@
 					$title = $infoArray[1];
 					$side = $infoArray[2];
 					$sideSay = ($side == "l" ? "left" : ($side == "r" ? "right" : "center"));
-
-					$end = "</div><br><br><br><br><br><br>";
 					$start = "<div style=\"text-align:$sideSay;\">";
 					if ($order == $lastOrder){$end = "";$start = "";}
-					$lastOrder = $order;
+					//$lastOrder = $order;
 					if($i==0){$lastOrder = $order;}
 					else{echo $end;}
 
 
 					if ($ext == "jpg" || $ext == "gif" || $ext == "png" || $ext == "jpeg"):	
-					if ($count == 3):			
+					if ($count == 3):		
+					//echo"'$order'";	
 				?>
-				<?php echo $start; ?><img style="padding:10px;" alt="<?php echo $title; ?>" src="<?php echo $fileURL; ?>">		
+				<?php echo $start; ?><img  id="<?echo$order;?>" class="piece"  style="padding:10px;" alt="<?php echo $title; ?>" src="<?php echo $fileURL; ?>">	
+				<div class="titles" id="_<?echo$order;?>"><?echo"<h3>$title</h3>";?></div>
 				<?php
 				echo"\n";
 				endif;
